d988ff1ab6aa28cd35fbd516054f0429b187dbb4
grislicepada <graceliemaecepada3@gmail.com>
2025-09-10 18:18:39 +0800

    Added student variables

---
+++ cepadaLife2.js
@@ -0,0 +1,5 @@
+let name = "Gracelie Cepada";
+let course = "Bachelor of Science in Information Technology";
+let year = "2nd Year";
+
+console.log ("\nName: "+name+"\nCourse: "+course+"\nYear Level: "+year);
