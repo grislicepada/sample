b2dbc7ff90b27db0511a4fcaa89ba33b9e85ed6e
grislicepada <graceliemaecepada3@gmail.com>
2025-09-10 18:22:55 +0800

    Added operators for daily routine

--- cepadaLife2.js
+++ cepadaLife2.js
@@ -3,3 +3,10 @@ let course = "Bachelor of Science in Information Technology";
 let year = "2nd Year";
 
 console.log ("\nName: "+name+"\nCourse: "+course+"\nYear Level: "+year);
+
+//operators
+
+let tlsleep = "8";
+let tlstudy = "2";
+
+console.log("\nTotal hours of sleep: "+tlsleep+"\nTotal hours of study: "+tlstudy);
